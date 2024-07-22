6820f351ee8860c74af88da213bf8dc18b217614
Constantin Goeldel <constantin_goeldel@t-online.de>
2024-07-22 16:46:52 +0200

    Experiments with the parser

--- redis/src/parser.rs
+++ redis/src/parser.rs
@@ -1,11 +1,11 @@
 use std::cmp::max;
-use nom::IResult;
+use nom::{Finish, IResult};
 use nom::branch::alt;
-use nom::bytes::complete::{is_not, tag, take};
-use nom::character::complete::char;
-use nom::combinator::{map_opt, opt};
+use nom::bytes::complete::{is_not, tag, take, digit1};
+use nom::character::complete::{char, digit1};
+use nom::combinator::{map_opt, map_res, opt};
 use nom::Err::Error;
-use nom::sequence::delimited;
+use nom::sequence::{delimited, tuple};
 
 struct Terminator;
 
@@ -14,9 +14,9 @@ impl Parse for Terminator {
         tag("\r\n")(i)
     }
 
-    fn parse(i: &[u8]) -> IResult<&[u8], Box<Self>> {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self> {
         let (i, _) = Self::find(i)?;
-        Ok((i, Box::new(Terminator)))
+        Ok((i, Terminator))
     }
 }
 
@@ -38,10 +38,10 @@ impl Parse for BulkString {
     /// Redis limits it to 512 MB (see the proto-max-bulk-len configuration directive).
     ///
     /// TODO: Implement the variable limit
-    fn parse(i: &[u8]) -> IResult<&[u8], Box<Self>> {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self> {
         let (i, data) = Self::find(i)?;
         let data = std::str::from_utf8(data).unwrap().to_string();
-        Ok((i, Box::new(BulkString(data))))
+        Ok((i, BulkString(data)))
     }
 }
 
@@ -52,9 +52,9 @@ impl Parse for NullBulkString {
         tag("$-1\r\n")(i)
     }
 
-    fn parse(i: &[u8]) -> IResult<&[u8], Box<Self>> {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self> {
         let (i, _) = Self::find(i)?;
-        Ok((i, Box::new(NullBulkString)))
+        Ok((i, NullBulkString))
     }
 }
 
@@ -69,7 +69,7 @@ impl Parse for Array {
 
         let mut j = i;
         for _ in 0..len {
-            let (k, _) = alt((BulkString::find, Integer::find, SimpleString::find, SimpleError::find, NullBulkString::find, Array::find)(j)?;
+            let (k, _) = alt((BulkString::find, Integer::find, SimpleString::find, SimpleError::find, NullBulkString::find, Array::find))(j)?;
             j = k;
         }
 
@@ -77,14 +77,14 @@ impl Parse for Array {
 
         Ok((j, &input[start_idx..end_idx]))
     }
-    fn parse(i: &[u8]) -> IResult<&[u8], Vec<&[u8]>> {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self> {
         let (i, array) = Self::find(i)?;
 
         let mut res: Vec<ParsedValues> = Vec::new();
         let mut j = array;
         while !j.is_empty() {
 
-            map_opt(Array::find,)
+            map_res(array, | )
 
 
 
@@ -115,61 +115,79 @@ enum ParsedValues {
 struct SimpleString(String);
 
 trait Parse {
-    fn parse(i: &[u8]) -> IResult<&[u8], Box<Self>>;
+    fn parse(i: &[u8]) -> IResult<&[u8], dyn Self>;
 
     fn find(i: &[u8]) -> IResult<&[u8], &[u8]>;
 }
 
 impl Parse for SimpleString  {
-    fn parse(i: &[u8]) -> IResult<&[u8], Box<Self>> {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self> {
         let (i, str) = Self::find(i)?;
-        Ok((i, Box::new(SimpleString(std::str::from_utf8(str).unwrap().to_string()))))
+        Ok((i, SimpleString(std::str::from_utf8(str).unwrap().to_string())))
     }
 
     fn find(i: &[u8]) -> IResult<&[u8], &[u8]> {
-        delimited(char('+'), is_not("\r\n"), terminator)(i)
+        delimited(char('+'), is_not("\r\n"), Terminator::find)(i)
     }
 }
 
 struct SimpleError(String);
 
 impl Parse for SimpleError {
-    fn parse(i: &[u8]) -> IResult<&[u8], Box<Self>> {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self> {
         let (i, str) = Self::find(i)?;
-        Ok((i, Box::new(SimpleError(std::str::from_utf8(str).unwrap().to_string()))))
+        Ok((i, SimpleError(std::str::from_utf8(str).unwrap().to_string())))
     }
 
     fn find(i: &[u8]) -> IResult<&[u8], &[u8]> {
-        delimited(char('-'), is_not("\r\n"), terminator)(i)
+        delimited(char('-'), is_not("\r\n"), Terminator::find)(i)
     }
 
 }
 
 struct Integer(i64);
 
-impl Parse for Integer {
-    fn find(i: &[u8]) -> IResult<&[u8], &[u8]> {
-        delimited(char(':'), is_not("\r\n"), terminator)(i)
-    }
+fn integer(i: &[u8]) -> IResult<&[u8], &[u8]> {
+    delimited(char(':'), is_not("\r\n"), Terminator::find)(i)
+}
 
-    fn parse(i: &[u8]) -> IResult<&[u8], Box<i64>> {
-        let (i, num) = Self::find(i)?;
+impl<'a > TryFrom<&'a [u8]> for Integer {
+    type Error = nom::error::Error<&'a [u8]>;
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        let (i, num) = integer(value)?;
 
-        fn sign (i: &[u8]) -> IResult<&[u8], char> {
-            alt((char('-'), char('+')))(i)
+        fn sign (i: &[u8]) -> IResult<&[u8], i64> {
+            map_res(alt((char('-'), char('+'))), |s| {
+                match s {
+                    '-' => Ok(-1),
+                    '+' => Ok(1),
+                    _ => unreachable!()
+                }})(i)
         }
 
-        let (j, sign) = opt(sign)(num)?;
+        fn parse_digits(i: &[u8]) -> IResult<&[u8], i64> {
+            map_res(digit1, str::parse)(i.as_char())
+        }
 
-        let sign = match sign {
-            Some('-') => -1,
-            Some('+') => 1,
-            None => 1,
-            _ => unreachable!()
-        };
+        let (j, (sign, digits))  = tuple((opt(sign),parse_digits))(num)?;
 
-        let num = std::str::from_utf8(j).unwrap().parse::<i64>().unwrap();
-        Ok((i, Box::new(sign * num)))
+        assert_eq!(j.len(), 0);
+
+        // If there is no explicit sign, assume positive int
+        let sign = sign.unwrap_or(1);
+
+        Ok(Integer(sign* digits))
+    }
+}
+
+
+
+impl Parse for Integer {
+    fn find(i: &[u8]) -> IResult<&[u8], &[u8]> {
+    }
+
+    fn parse(i: &[u8]) -> IResult<&[u8], i64> {
+
 
     }
 }
@@ -182,7 +200,7 @@ mod test {
     #[test]
     fn test_simple_string() {
         let s = b"+OK\r\n";
-        let (i, str) = simple_string(s).unwrap();
+        let (i, str) = SimpleString::parse(s).unwrap();
         assert_eq!(i, b"");
         assert_eq!(str, b"OK");
     }
@@ -190,7 +208,7 @@ mod test {
     #[test]
     fn test_simple_error() {
         let s = b"-Error message\r\n";
-        let (i, str) = simple_error(s).unwrap();
+        let (i, str) = SimpleError::parse(s).unwrap();
         assert_eq!(i, b"");
         assert_eq!(str, b"Error message");
     }
@@ -198,15 +216,14 @@ mod test {
     #[test]
     fn test_integer() {
         let s = b":1000\r\n";
-        let (i, num) = integer(s).unwrap();
-        assert_eq!(i, b"");
+        let num = Integer::try_from(s).unwrap();
         assert_eq!(num, 1000);
     }
 
     #[test]
     fn test_integer_negative() {
         let s = b":-1000\r\n";
-        let (i, num) = integer(s).unwrap();
+        let (i, num) = Integer::parse(s).unwrap();
         assert_eq!(i, b"");
         assert_eq!(num, -1000);
     }
@@ -214,7 +231,7 @@ mod test {
     #[test]
     fn test_integer_positive() {
         let s = b":+1000\r\n";
-        let (i, num) = integer(s).unwrap();
+        let (i, num) = Integer::parse(s).unwrap();
         assert_eq!(i, b"");
         assert_eq!(num, 1000);
     }
@@ -222,7 +239,7 @@ mod test {
     #[test]
     fn test_bulk_string() {
         let s = b"$6\r\nfoobar\r\n";
-        let (i, str) = bulk_string(s).unwrap();
+        let (i, str) = BulkString::parse(s).unwrap();
         assert_eq!(i, b"");
         assert_eq!(str, b"foobar");
     }
@@ -230,7 +247,7 @@ mod test {
     #[test]
     fn test_empty_bulk_string() {
         let s = b"$0\r\n\r\n";
-        let (i, str) = bulk_string(s).unwrap();
+        let (i, str) = BulkString::parse(s).unwrap();
         assert_eq!(i, b"");
         assert_eq!(str, b"");
     }
@@ -238,7 +255,7 @@ mod test {
     #[test]
     fn test_bulk_string_too_large() {
         let s = b"$536870913\r\n";
-        let r = bulk_string(s);
+        let r = BulkString::parse(s);
         assert!(r.is_err());
     }
 
@@ -246,13 +263,13 @@ mod test {
     #[should_panic]
     fn test_bulk_string_with_wrong_size() {
         let s = b"$6\r\nfoobarssss\r\n";
-        let r = bulk_string(s).unwrap();
+        let r = BulkString::parse(s).unwrap();
     }
 
     #[test]
     fn test_null_bulk_string() {
         let s = b"$-1\r\n";
-        let (i, res) = null_bulk_string(s).unwrap();
+        let (i, res) = NullBulkString::parse(s).unwrap();
         assert_eq!(i, b"");
         assert_eq!(res, ());
     }
@@ -260,7 +277,7 @@ mod test {
     #[test]
     fn test_empty_array() {
         let s = b"*0\r\n";
-        let (i, res) = array(s).unwrap();
+        let (i, res) = Array::parse(s).unwrap();
         assert_eq!(i, b"");
         assert_eq!(res.len(), 0);
     }
@@ -268,7 +285,7 @@ mod test {
     #[test]
     fn test_string_array() {
         let s = b"*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n";
-        let (i, res) = array(s).unwrap();
+        let (i, res) = Array::parse(s).unwrap();
         assert_eq!(i, b"");
         assert_eq!(res.len(), 2);
         assert_eq!(res[0], b"hello");
@@ -278,7 +295,7 @@ mod test {
     #[test]
     fn test_int_array() {
         let s = b"*3\r\n:1\r\n:2\r\n:3\r\n";
-        let (i, res) = array(s).unwrap();
+        let (i, res) = Array::parse(s).unwrap();
         assert_eq!(i, b"");
         assert_eq!(res.len(), 3);
         assert_eq!(res[0], b"1");
@@ -289,7 +306,7 @@ mod test {
     #[test]
     fn test_mixed_array() {
         let s  = b"*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$5\r\nhello\r\n";
-        let (i, res) = array(s).unwrap();
+        let (i, res) = Array::parse(s).unwrap();
 
         assert_eq!(i, b"");
         assert_eq!(res.len(), 5);
